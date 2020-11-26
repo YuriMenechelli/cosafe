ef2014a91a27ec71e95f6bcbac724ba81acf394e
kezleyunip <75053751+kezleyunip@users.noreply.github.com>
2020-11-25 23:33:07 -0300

    Update Usuario.php

--- models/Usuario.php
+++ models/Usuario.php
@@ -102,14 +102,14 @@ use PHPMailer\PHPMailer\Exception;
 
 				try {
 				    //Server settings
-				    //$mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
-				    $mail->isSMTP();                                            // Send using SMTP
-				    $mail->Host       = 'smtp.mailtrap.io';                    // Set the SMTP server to send through
-				    $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-				    $mail->Username   = '4c1d4a3f451b19';                     				// SMTP username
-				    $mail->Password   = '4608742403faf5';                               		// SMTP password
-				    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
-				    $mail->Port       = 2525;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+				    //$mail->SMTPDebug = SMTP::DEBUG_SERVER;                     
+				    $mail->isSMTP();                                            
+				    $mail->Host       = 'smtp.mailtrap.io';                    
+				    $mail->SMTPAuth   = true;                                   
+				    $mail->Username   = '';                     				
+				    $mail->Password   = '';                               		
+				    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         
+				    $mail->Port       = 2525;                                    
 
 				    //Recipients
 				    $mail->setFrom('Suporte@cosafe.com', 'CO-SAFE');
@@ -118,7 +118,7 @@ use PHPMailer\PHPMailer\Exception;
 
 
 				    // Content
-				    $mail->isHTML(true);                                  // Set email format to HTML
+				    $mail->isHTML(true);                                  
 				    $mail->Subject = 'CO-SAFE';
 				    $mail->Body    = utf8_decode($message);
 
